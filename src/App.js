1bf0da028097028b782bcf9c5b7f15132c23407b
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-10-18 18:11:36 -0400

    Edited app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
+function DisplayCard({ quote }) {
+  const [captainLogs, setCaptainLogs] = useState([]);
 
-useEffect {
-  const [logs, SetLogs] = useState();
+  useEffect(() => {
+
+    fetch({ origin: "https://localhost:6116/" })
+      .then((response) => response.json())
+      .then((data) => setCaptainLogs(data))
+      .catch((error) => console.error("Error fetching data", error));
+  }, []);
+
+  const captainsLogReportRender = captainLogs.map((eachCaptainQuote) => {
+    const loggedQuote = quote.some((saying) => saying.id === eachCaptainQuote.id)
+  }
+  
+    return (
+      <h4>
+        <div className="captainName"> {`${eachCaptainQuote.name}`} </> 
+
+
+        </h4>
+
+
+          < div className="title" > {`${eachCaptainQuote.title}`} </div>
+
+  )
 
 
 
-
-
-}
-
-
-function App() {
-  return <div>Hello World</div>;
-}
-
-export default App;
+  export default App;
